f8cb4ff4fe390733729bf5d52a53aadd1396f2c8
Norton Glitz <norton.glitz@gmail.com>
2023-12-05 23:59:31 -0300

    trying removing async from usePlayersOnline

--- app/hooks/usePlayersOnline.ts
+++ app/hooks/usePlayersOnline.ts
@@ -1,11 +1,9 @@
 "use client"
 
-export const dynamic = 'force-dynamic'
-
 import useSWR from 'swr'
 
 export function usePlayersOnline() {
-    const { data, isLoading, error } = useSWR('/api/playersHistory/latest', async (...args) => {
+    const { data, isLoading, error } = useSWR('/api/playersHistory/latest', (...args) => {
         return fetch(...args).then(res => res.json())
     })
 
