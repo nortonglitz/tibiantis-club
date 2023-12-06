5a79560289d068104efb81cfe26baa8f2caca527
Norton Glitz <norton.glitz@gmail.com>
2023-12-06 01:12:07 -0300

    trying no cache again on fetch

--- app/hooks/usePlayersOnline.ts
+++ app/hooks/usePlayersOnline.ts
@@ -4,7 +4,7 @@ import useSWR from 'swr'
 
 export function usePlayersOnline() {
 
-    const fetcher = (url: string) => fetch(url).then(res => res.json())
+    const fetcher = (url: string) => fetch(url, { cache: "no-store" }).then(res => res.json())
 
     const { data, isLoading, error } = useSWR('/api/playersHistory/latest', fetcher)
 
