b1fc0007b0bb3bc603ed2bd50aec318f40b80c9b
Norton Glitz <norton.glitz@gmail.com>
2023-12-06 03:23:55 -0300

    another try

--- app/hooks/usePlayersOnline.ts
+++ app/hooks/usePlayersOnline.ts
@@ -6,7 +6,10 @@ export function usePlayersOnline() {
 
     const fetcher = (url: string) => fetch(url).then(res => res.json())
     const { data, isLoading, error } = useSWR('/api/playersHistory/latest', fetcher, {
-        dedupingInterval: 120000
+        dedupingInterval: 120000,
+        focusThrottleInterval: 120000,
+        revalidateOnFocus: false,
+        revalidateOnMount: false
     })
 
     return { quantity: data?.quantity, isLoading, error }
