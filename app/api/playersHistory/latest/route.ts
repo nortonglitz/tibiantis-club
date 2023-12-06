6b1b321fc29471b3f7c697440dc3f2266d6e4253
Norton Glitz <norton.glitz@gmail.com>
2023-12-06 01:29:51 -0300

    changing api to be dynamic

--- app/api/playersHistory/latest/route.ts
+++ app/api/playersHistory/latest/route.ts
@@ -1,5 +1,7 @@
 import { prisma } from '@/app/libs/dbClient'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
     try {
         const latestRegister = await prisma.playersHistory.findFirst({ orderBy: { id: "desc" } })
