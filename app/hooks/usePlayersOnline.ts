7ba6c7fd7953c0b46dfd57f479cc1ce674474504
Norton Glitz <norton.glitz@gmail.com>
2023-12-05 23:44:38 -0300

    trying force dynamic on usePlayersOnline

--- app/hooks/usePlayersOnline.ts
+++ app/hooks/usePlayersOnline.ts
@@ -1,10 +1,12 @@
 "use client"
 
+export const dynamic = 'force-dynamic'
+
 import useSWR from 'swr'
 
 export function usePlayersOnline() {
     const { data, isLoading, error } = useSWR('/api/playersHistory/latest', async (...args) => {
-        return fetch(...args, { cache: 'no-store' }).then(res => res.json())
+        return fetch(...args).then(res => res.json())
     })
 
     console.log(data)
