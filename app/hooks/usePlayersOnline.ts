810207f7076899af581b4b040fbd88703a450630
Norton Glitz <norton.glitz@gmail.com>
2023-12-05 23:30:14 -0300

    bug check usePlayersOnline hook

--- app/hooks/usePlayersOnline.ts
+++ app/hooks/usePlayersOnline.ts
@@ -3,10 +3,11 @@
 import useSWR from 'swr'
 
 export function usePlayersOnline() {
-    console.log('Am I being printed?')
     const { data, isLoading, error } = useSWR('/api/playersHistory/latest', async (...args) => {
         return fetch(...args).then(res => res.json())
     })
+
+    console.log(data)
 
     return { quantity: data?.quantity, isLoading, error }
 }
