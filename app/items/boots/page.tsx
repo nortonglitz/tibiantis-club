4b616f012fe15d796c93e89da7c4634c45f6abb9
Norton Glitz <norton.glitz@gmail.com>
2023-12-16 17:21:08 -0300

    boots page working

---
+++ app/items/boots/page.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import Table from "./table"
+
+export const metadata: Metadata = {
+    title: 'Boots - Tibiantis Club',
+}
+
+export default function FoodsPage() {
+
+    return (
+        <main
+            className="
+                flex
+                justify-center
+                py-8
+                sm:p-10
+            "
+        >
+            <Table />
+        </main>
+    )
+}
---
+++ prisma/seeds/boots.js
@@ -0,0 +1,50 @@
+const boots = [
+    {
+        name: "boots of haste",
+        attr: "speed +20",
+        weight: 7.5,
+        imageSrc: '/assets/imgs/boots/boots_of_haste.gif'
+    },
+    {
+        name: "bunnyslippers",
+        weight: 6.0,
+        imageSrc: '/assets/imgs/boots/bunnyslippers.gif'
+    },
+    {
+        name: "golden boots",
+        arm: 8,
+        weight: 32.0,
+        imageSrc: '/assets/imgs/boots/golden_boots.gif'
+    },
+    {
+        name: "leather boots",
+        arm: 1,
+        weight: 9.0,
+        imageSrc: '/assets/imgs/boots/leather_boots.gif'
+    },
+    {
+        name: "patched boots",
+        arm: 2,
+        weight: 10.0,
+        imageSrc: '/assets/imgs/boots/patched_boots.gif'
+    },
+    {
+        name: "sandals",
+        weight: 6.0,
+        imageSrc: '/assets/imgs/boots/sandals.gif'
+    },
+    {
+        name: "soft boots",
+        attr: "faster regeneration",
+        weight: 10.0,
+        imageSrc: '/assets/imgs/boots/soft_boots.gif'
+    },
+    {
+        name: "steel boots",
+        arm: 5,
+        weight: 29.0,
+        imageSrc: '/assets/imgs/boots/steel_boots.gif'
+    }
+]
+
+module.exports = boots
