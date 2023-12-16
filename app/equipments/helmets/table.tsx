4721bb6c32fe9bddbc34957fe2b691ec08b2f8ea
Norton Glitz <norton.glitz@gmail.com>
2023-12-16 19:35:39 -0300

    helmets page working

---
+++ app/equipments/helmets/table.tsx
@@ -0,0 +1,134 @@
+"use client"
+
+import { useState } from "react"
+import helmetsData from '../../../prisma/seeds/helmets'
+
+import { FaSortAlphaDown, FaSortAlphaUp, FaSortNumericDown, FaSortNumericUp } from "react-icons/fa"
+
+type Field = "name" | "attr" | "weight" | "arm"
+type Order = "asc" | "desc"
+
+const Table: React.FC = () => {
+
+    const [helmets, setHelmets] = useState([...helmetsData].sort((a, b) => a.name.localeCompare(b.name)))
+    const [sortProps, setSortProps] = useState<{ field: Field, order: Order }>({
+        field: 'name',
+        order: 'asc'
+    })
+
+    const handleSort = (field: Field) => {
+        if (field === 'name') {
+            if (sortProps.order === 'desc') {
+                setHelmets([...helmets].sort((a, b) => a.name.localeCompare(b.name)))
+                setSortProps({ field: 'name', order: 'asc' })
+            } else {
+                setHelmets([...helmets].sort((a, b) => b.name.localeCompare(a.name)))
+                setSortProps({ field: 'name', order: 'desc' })
+            }
+        }
+
+        if (field === 'arm') {
+            if (sortProps.order === 'desc') {
+                setHelmets([...helmets].sort((a, b) => !a.arm ? -1 : !b.arm ? 1 : a.arm - b.arm))
+                setSortProps({ field: 'arm', order: 'asc' })
+            } else {
+                setHelmets([...helmets].sort((a, b) => !a.arm ? 1 : !b.arm ? -1 : b.arm - a.arm))
+                setSortProps({ field: 'arm', order: 'desc' })
+            }
+        }
+
+        if (field === 'weight') {
+            if (sortProps.order === 'desc') {
+                setHelmets([...helmets].sort((a, b) => a.weight - b.weight))
+                setSortProps({ field: 'weight', order: 'asc' })
+            } else {
+                setHelmets([...helmets].sort((a, b) => b.weight - a.weight))
+                setSortProps({ field: 'weight', order: 'desc' })
+            }
+        }
+    }
+
+    return (
+        <div className="px-3 pt-1 bg-stone-800 rounded-3xl border border-stone-200/10 w-full sm:w-fit">
+            <div className="max-h-[80vh] w-full sm:w-[75vw] overflow-y-auto rounded-xl">
+                <table className="relative text-center w-full">
+                    <thead className="top-0 sticky z-10">
+                        <tr
+                            className="
+                                    self-end
+                                    shadow-md
+                                    [&>th]:py-1
+                                    uppercase
+                                    font-yatra-one
+                                    bg-stone-800
+                                    select-none
+                                "
+                        >
+                            <th scope="col" className="min-w-[40px]"></th>
+                            <th className="min-w-[100px] cursor-pointer" scope="col" onClick={() => handleSort('name')}>
+                                <div className="w-fit relative m-auto [&>svg]:hidden sm:[&>svg]:block  [&>svg]:absolute [&>svg]:top-[0.1rem] [&>svg]:-right-6">
+                                    Name
+                                    {sortProps.field === 'name' ? sortProps.order === 'asc' ?
+                                        <FaSortAlphaDown /> : <FaSortAlphaUp />
+                                        : null
+                                    }
+                                </div>
+                            </th>
+                            <th className="min-w-[100px] cursor-pointer" scope="col" onClick={() => handleSort('arm')}>
+                                <div className="w-fit relative m-auto [&>svg]:hidden sm:[&>svg]:block [&>svg]:absolute [&>svg]:top-[0.1rem] [&>svg]:-right-6">
+                                    Arm
+                                    {sortProps.field === 'arm' ? sortProps.order === 'asc' ?
+                                        <FaSortNumericDown /> : <FaSortNumericUp />
+                                        : null
+                                    }
+                                </div>
+                            </th>
+                            <th className="min-w-[100px]" scope="col">
+                                Attr
+                            </th>
+                            <th className="text-stone-500 min-w-[100px] cursor-pointer" scope="col" onClick={() => handleSort("weight")}>
+                                <div className="w-fit relative m-auto [&>svg]:hidden sm:[&>svg]:block [&>svg]:absolute [&>svg]:top-[0.1rem] [&>svg]:-right-6">
+                                    Weight
+                                    {sortProps.field === 'weight' ? sortProps.order === 'asc' ?
+                                        <FaSortNumericDown /> : <FaSortNumericUp />
+                                        : null
+                                    }
+                                </div>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {helmets.map(({ weight, name, imageSrc, arm, attr }, i) => (
+                            <tr
+                                key={i}
+                                className="
+                                        [&>td]:py-2
+                                        odd:bg-stone-400/10 
+                                        even:bg-stone-300/10
+                                        hover:outline
+                                        hover:-outline-offset-1
+                                        hover:outline-stone-400/80
+                                    "
+                            >
+                                <td>
+                                    <img className="m-auto" src={imageSrc} height={32} width={32} alt={name} />
+                                </td>
+                                <td className="text-left capitalize text-base sm:text-lg">{name}</td>
+                                <td>{arm}</td>
+                                <td>{attr}</td>
+                                <td className="text-stone-500">{weight} oz</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                    <tfoot>
+                        <tr className="sticky bottom-0 bg-stone-800 italic text-sm pb-1 z-10" >
+                            <td colSpan={5} className="py-2" />
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+export default Table
---
+++ prisma/seeds/helmets.js
@@ -0,0 +1,209 @@
+const helmets = [
+    {
+        name: "amazon helmet",
+        arm: 7,
+        weight: 29.5,
+        imageSrc: "/assets/imgs/helmets/amazon_helmet.gif"
+    },
+    {
+        name: "ancient tiara",
+        weight: 8.2,
+        imageSrc: "/assets/imgs/helmets/ancient_tiara.gif"
+    },
+    {
+        name: "brass helmet",
+        arm: 3,
+        weight: 27.0,
+        imageSrc: "/assets/imgs/helmets/brass_helmet.gif"
+    },
+    {
+        name: "ceremonial mask",
+        arm: 10,
+        weight: 40.0,
+        imageSrc: "/assets/imgs/helmets/ceremonial_mask.gif"
+    },
+    {
+        name: "chain helmet",
+        arm: 2,
+        weight: 42.0,
+        imageSrc: "/assets/imgs/helmets/chain_helmet.gif"
+    },
+    {
+        name: "crown",
+        weight: 19.0,
+        imageSrc: "/assets/imgs/helmets/crown.gif"
+    },
+    {
+        name: "crown helmet",
+        arm: 7,
+        weight: 29.5,
+        imageSrc: "/assets/imgs/helmets/crown_helmet.gif"
+    },
+    {
+        name: "crusader helmet",
+        arm: 9,
+        weight: 52.0,
+        imageSrc: "/assets/imgs/helmets/crusader_helmet.gif"
+    },
+    {
+        name: "dark helmet",
+        arm: 6,
+        weight: 46.0,
+        imageSrc: "/assets/imgs/helmets/dark_helmet.gif"
+    },
+    {
+        name: "demon helmet",
+        arm: 13,
+        weight: 29.5,
+        imageSrc: "/assets/imgs/helmets/demon_helmet.gif"
+    },
+    {
+        name: "devil helmet",
+        arm: 7,
+        weight: 50.0,
+        imageSrc: "/assets/imgs/helmets/devil_helmet.gif"
+    },
+    {
+        name: "dragon scale helmet",
+        arm: 11,
+        weight: 32.5,
+        imageSrc: "/assets/imgs/helmets/dragon_scale_helmet.gif"
+    },
+    {
+        name: "dwarven helmet",
+        arm: 6,
+        weight: 42.0,
+        imageSrc: "/assets/imgs/helmets/dwarven_helmet.gif"
+    },
+    {
+        name: "golden helmet",
+        arm: 17,
+        weight: 32.0,
+        imageSrc: "/assets/imgs/helmets/golden_helmet.gif"
+    },
+    {
+        name: "hat of the mad",
+        arm: 3,
+        weight: 7.0,
+        imageSrc: "/assets/imgs/helmets/hat_of_the_mad.gif"
+    },
+    {
+        name: "helmet of the ancients",
+        arm: 11,
+        weight: 27.6,
+        imageSrc: "/assets/imgs/helmets/helmet_of_the_ancients.gif"
+    },
+    {
+        name: "? helmet of the ancients",
+        imageSrc: "/assets/imgs/helmets/helmet_of_the_ancients_ruby.gif"
+    },
+    {
+        name: "? helmet of the ancients",
+        imageSrc: "/assets/imgs/helmets/helmet_of_the_ancients_emerald.gif"
+    },
+    {
+        name: "? helmet of the ancients",
+        imageSrc: "/assets/imgs/helmets/helmet_of_the_ancients_amethyst.gif"
+    },
+    {
+        name: "? helmet of the ancients",
+        imageSrc: "/assets/imgs/helmets/helmet_of_the_ancients_sapphire.gif"
+    },
+    {
+        name: "horned helmet",
+        arm: 16,
+        weight: 51.0,
+        imageSrc: "/assets/imgs/helmets/horned_helmet.gif"
+    },
+    {
+        name: "iron helmet",
+        arm: 5,
+        weight: 30.0,
+        imageSrc: "/assets/imgs/helmets/iron_helmet.gif"
+    },
+    {
+        name: "leather helmet",
+        arm: 1,
+        weight: 22.0,
+        imageSrc: "/assets/imgs/helmets/leather_helmet.gif"
+    },
+    {
+        name: "legion helmet",
+        arm: 4,
+        weight: 31.0,
+        imageSrc: "/assets/imgs/helmets/legion_helmet.gif"
+    },
+    {
+        name: "mystic turban",
+        arm: 1,
+        weight: 8.5,
+        imageSrc: "/assets/imgs/helmets/mystic_turban.gif"
+    },
+    {
+        name: "post officer's hat",
+        arm: 1,
+        weight: 7.0,
+        imageSrc: "/assets/imgs/helmets/post_officers_hat.gif"
+    },
+    {
+        name: "royal helmet",
+        arm: 11,
+        weight: 48.0,
+        imageSrc: "/assets/imgs/helmets/royal_helmet.gif"
+    },
+    {
+        name: "soldier helmet",
+        arm: 5,
+        weight: 32.0,
+        imageSrc: "/assets/imgs/helmets/soldier_helmet.gif"
+    },
+    {
+        name: "steel helmet",
+        arm: 6,
+        weight: 46.0,
+        imageSrc: "/assets/imgs/helmets/steel_helmet.gif"
+    },
+    {
+        name: "strange helmet",
+        arm: 6,
+        weight: 46.0,
+        imageSrc: "/assets/imgs/helmets/strange_helmet.gif"
+    },
+    {
+        name: "studded helmet",
+        arm: 2,
+        weight: 24.5,
+        imageSrc: "/assets/imgs/helmets/studded_helmet.gif"
+    },
+    {
+        name: "viking helmet",
+        arm: 4,
+        weight: 39.0,
+        imageSrc: "/assets/imgs/helmets/viking_helmet.gif"
+    },
+    {
+        name: "warrior helmet",
+        arm: 9,
+        weight: 68.0,
+        imageSrc: "/assets/imgs/helmets/warrior_helmet.gif"
+    },
+    {
+        name: "winged helmet",
+        arm: 14,
+        attr: "speed +40",
+        weight: 12.0,
+        imageSrc: "/assets/imgs/helmets/winged_helmet.gif"
+    },
+    {
+        name: "wood cape",
+        arm: 2,
+        weight: 11.0,
+        imageSrc: "/assets/imgs/helmets/wood_cape.gif"
+    },
+    {
+        name: "?",
+        imageSrc: "/assets/imgs/helmets/6705.gif"
+    }
+]
+
+module.exports = helmets
