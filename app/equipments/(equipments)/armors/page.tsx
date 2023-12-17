3530fb628e8142eb683e0be1869f7b840c369393
Norton Glitz <norton.glitz@gmail.com>
2023-12-17 17:56:02 -0300

    armors page working

---
+++ app/equipments/(equipments)/armors/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from 'next'
+import Table from './table'
+
+export const metadata: Metadata = {
+    title: 'Armors - Tibiantis Club',
+}
+
+export default function ArmorsPage() {
+
+    return (
+        <Table />
+    )
+}
---
+++ prisma/seeds/armors.js
@@ -0,0 +1,184 @@
+const armors = [
+    {
+        name: "amazon armor",
+        arm: 13,
+        weight: 45.0,
+        imageSrc: "/assets/imgs/armors/amazon_armor.gif"
+    },
+    {
+        name: "blue robe",
+        arm: 11,
+        weight: 22.0,
+        imageSrc: "/assets/imgs/armors/blue_robe.gif"
+    },
+    {
+        name: "brass armor",
+        arm: 8,
+        weight: 80.0,
+        imageSrc: "/assets/imgs/armors/brass_armor.gif"
+    },
+    {
+        name: "cape",
+        arm: 1,
+        weight: 32.0,
+        imageSrc: "/assets/imgs/armors/cape.gif"
+    },
+    {
+        name: "chain armor",
+        arm: 6,
+        weight: 100.0,
+        imageSrc: "/assets/imgs/armors/chain_armor.gif"
+    },
+    {
+        name: "coat",
+        arm: 1,
+        weight: 27.0,
+        imageSrc: "/assets/imgs/armors/coat.gif"
+    },
+    {
+        name: "crown armor",
+        arm: 15,
+        weight: 99.0,
+        imageSrc: "/assets/imgs/armors/crown_armor.gif"
+    },
+    {
+        name: "dark armor",
+        arm: 10,
+        weight: 120.0,
+        imageSrc: "/assets/imgs/armors/dark_armor.gif"
+    },
+    {
+        name: "demon armor",
+        arm: 21,
+        attr: "3% fire protection",
+        weight: 80.0,
+        imageSrc: "/assets/imgs/armors/demon_armor.gif"
+    },
+    {
+        name: "doublet",
+        arm: 2,
+        weight: 25.0,
+        imageSrc: "/assets/imgs/armors/doublet.gif"
+    },
+    {
+        name: "dragon scale mail",
+        arm: 19,
+        weight: 114.0,
+        imageSrc: "/assets/imgs/armors/dragon_scale_mail.gif"
+    },
+    {
+        name: "dwarven armor",
+        arm: 10,
+        weight: 130.0,
+        imageSrc: "/assets/imgs/armors/dwarven_armor.gif"
+    },
+    {
+        name: "elven mail",
+        arm: 9,
+        weight: 90.0,
+        imageSrc: "/assets/imgs/armors/elven_mail.gif"
+    },
+    {
+        name: "golden armor",
+        arm: 17,
+        attr: "shining",
+        weight: 80.0,
+        imageSrc: "/assets/imgs/armors/golden_armor.gif"
+    },
+    {
+        name: "green tunic",
+        arm: 1,
+        weight: 9.3,
+        imageSrc: "/assets/imgs/armors/green_tunic.gif"
+    },
+    {
+        name: "jacket",
+        arm: 1,
+        weight: 24.0,
+        imageSrc: "/assets/imgs/armors/jacket.gif"
+    },
+    {
+        name: "knight armor",
+        arm: 13,
+        weight: 120.0,
+        imageSrc: "/assets/imgs/armors/knight_armor.gif"
+    },
+    {
+        name: "leather armor",
+        arm: 4,
+        weight: 60.0,
+        imageSrc: "/assets/imgs/armors/leather_armor.gif"
+    },
+    {
+        name: "magic plate armor",
+        arm: 22,
+        attr: "30% mana drain protection",
+        weight: 85.0,
+        imageSrc: "/assets/imgs/armors/magic_plate_armor.gif"
+    },
+    {
+        name: "native armor",
+        arm: 7,
+        weight: 80.0,
+        imageSrc: "/assets/imgs/armors/native_armor.gif"
+    },
+    {
+        name: "noble armor",
+        arm: 11,
+        weight: 120.0,
+        imageSrc: "/assets/imgs/armors/noble_armor.gif"
+    },
+    {
+        name: "plate armor",
+        arm: 10,
+        weight: 120.0,
+        imageSrc: "/assets/imgs/armors/plate_armor.gif"
+    },
+    {
+        name: "ranger's cloak",
+        arm: 7,
+        weight: 48.0,
+        imageSrc: "/assets/imgs/armors/rangers_cloak.gif"
+    },
+    {
+        name: "red robe",
+        arm: 1,
+        weight: 26.0,
+        imageSrc: "/assets/imgs/armors/red_robe.gif"
+    },
+    {
+        name: "red tunic",
+        arm: 2,
+        weight: 14.0,
+        imageSrc: "/assets/imgs/armors/red_tunic.gif"
+    },
+    {
+        name: "scale armor",
+        arm: 9,
+        weight: 105.0,
+        imageSrc: "/assets/imgs/armors/scale_armor.gif"
+    },
+    {
+        name: "simple dress",
+        weight: 24.0,
+        imageSrc: "/assets/imgs/armors/simple_dress.gif"
+    },
+    {
+        name: "studded armor",
+        arm: 5,
+        weight: 71.0,
+        imageSrc: "/assets/imgs/armors/studded_armor.gif"
+    },
+    {
+        name: "white dress",
+        weight: 24.0,
+        imageSrc: "/assets/imgs/armors/white_dress.gif"
+    },
+    {
+        name: "ball gown",
+        weight: 25.0,
+        imageSrc: "/assets/imgs/armors/ball_gown.gif"
+    },
+]
+
+module.exports = armors
