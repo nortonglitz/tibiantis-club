4939fdb92c944790849b979299b951e4e31d1ed1
Norton Glitz <norton.glitz@gmail.com>
2023-12-17 02:49:22 -0300

    not found page with exit button

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import Link from "next/link"
+
 export default function ErrorPage() {
     return (
         <main className="pt-10 flex justify-center px-4">
@@ -15,6 +17,28 @@ export default function ErrorPage() {
                     <div className="flex justify-around mt-5">
                         <img src="/assets/not_found_body_male.gif" alt="dead man" />
                         <img src="/assets/not_found_body_female.gif" alt="dead woman" />
+                    </div>
+                    <div className="flex justify-center mt-4">
+                        <Link
+                            href="/"
+                            replace
+                            className="
+                                transition
+                                border
+                                border-red-600/20
+                                bg-red-600/10
+                                hover:bg-red-600/30
+                                active:bg-red-600/40
+                                py-2 
+                                px-3 
+                                rounded-full
+                            "
+                        >
+                            <div className="flex items-center gap-2">
+                                <img className="animate-bounce" src="/assets/imgs/signs/west.gif" alt="sign" />
+                                Get out!
+                            </div>
+                        </Link>
                     </div>
                 </div>
             </div>
