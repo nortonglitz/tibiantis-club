b672067c0362d9e0366453be465f29f9b45293ef
Norton Glitz <norton.glitz@gmail.com>
2023-12-17 21:40:24 -0300

    shields page working

--- app/equipments/(equipments)/equipmentsLinks.tsx
+++ app/equipments/(equipments)/equipmentsLinks.tsx
@@ -44,7 +44,7 @@ const EquipmentLink: React.FC<EquipmentLinkProps> = ({
                 <div
                     className={`
                         absolute 
-                        top-0 
+                        top-0
                         left-0 
                         w-[32px] 
                         h-[32px]
@@ -60,7 +60,7 @@ const EquipmentLink: React.FC<EquipmentLinkProps> = ({
 
 const EquipmentsLinks = () => {
     return (
-        <div className="hidden sm:flex sm:flex-col absolute top-32 left-20">
+        <div className="hidden sm:flex sm:flex-col absolute lg:top-32 md:top-28 sm:top-20 lg:left-16 md:left-8 sm:left-3">
             <EquipmentLink category="swords" imgSrc="/assets/imgs/swords/magic_sword.gif" />
             <EquipmentLink category="clubs" imgSrc="/assets/imgs/clubs/thunder_hammer.gif" />
             <EquipmentLink category="axes" imgSrc="/assets/imgs/axes/stonecutter_axe.gif" />
---
+++ prisma/seeds/shields.js
@@ -0,0 +1,208 @@
+const shields = [
+    {
+        name: "amazon shield",
+        def: 32,
+        weight: 62.0,
+        imageSrc: "/assets/imgs/shields/amazon_shield.gif"
+    },
+    {
+        name: "ancient shield",
+        def: 27,
+        weight: 61.0,
+        imageSrc: "/assets/imgs/shields/ancient_shield.gif"
+    },
+    {
+        name: "battle shield",
+        def: 23,
+        weight: 62.0,
+        imageSrc: "/assets/imgs/shields/battle_shield.gif"
+    },
+    {
+        name: "beholder shield",
+        def: 28,
+        weight: 47.0,
+        imageSrc: "/assets/imgs/shields/beholder_shield.gif"
+    },
+    {
+        name: "black shield",
+        def: 18,
+        weight: 41.5,
+        imageSrc: "/assets/imgs/shields/black_shield.gif"
+    },
+    {
+        name: "blessed shield",
+        def: 40,
+        weight: 68.0,
+        imageSrc: "/assets/imgs/shields/blessed_shield.gif"
+    },
+    {
+        name: "bone shield",
+        def: 20,
+        weight: 55.0,
+        imageSrc: "/assets/imgs/shields/bone_shield.gif"
+    },
+    {
+        name: "brass shield",
+        def: 16,
+        weight: 90.0,
+        imageSrc: "/assets/imgs/shields/brass_shield.gif"
+    },
+    {
+        name: "castle shield",
+        def: 28,
+        weight: 49.0,
+        imageSrc: "/assets/imgs/shields/castle_shield.gif"
+    },
+    {
+        name: "copper shield",
+        def: 19,
+        weight: 63.0,
+        imageSrc: "/assets/imgs/shields/copper_shield.gif"
+    },
+    {
+        name: "crown shield",
+        def: 32,
+        weight: 62.0,
+        imageSrc: "/assets/imgs/shields/crown_shield.gif"
+    },
+    {
+        name: "dark shield",
+        def: 25,
+        weight: 52.0,
+        imageSrc: "/assets/imgs/shields/dark_shield.gif"
+    },
+    {
+        name: "demon shield",
+        def: 35,
+        weight: 26.0,
+        imageSrc: "/assets/imgs/shields/demon_shield.gif"
+    },
+    {
+        name: "dragon shield",
+        def: 31,
+        weight: 60.0,
+        imageSrc: "/assets/imgs/shields/dragon_shield.gif"
+    },
+    {
+        name: "dwarven shield",
+        def: 26,
+        weight: 55.0,
+        imageSrc: "/assets/imgs/shields/dwarven_shield.gif"
+    },
+    {
+        name: "eagle shield",
+        def: 32,
+        weight: 62.0,
+        imageSrc: "/assets/imgs/shields/eagle_shield.gif"
+    },
+    {
+        name: "great shield",
+        def: 38,
+        weight: 84.0,
+        imageSrc: "/assets/imgs/shields/great_shield.gif"
+    },
+    {
+        name: "griffin shield",
+        def: 29,
+        weight: 50.0,
+        imageSrc: "/assets/imgs/shields/griffin_shield.gif"
+    },
+    {
+        name: "guardian shield",
+        def: 30,
+        weight: 55.0,
+        imageSrc: "/assets/imgs/shields/guardian_shield.gif"
+    },
+    {
+        name: "mastermind shield",
+        def: 37,
+        weight: 57.0,
+        imageSrc: "/assets/imgs/shields/mastermind_shield.gif"
+    },
+    {
+        name: "medusa shield",
+        def: 33,
+        weight: 58.0,
+        imageSrc: "/assets/imgs/shields/medusa_shield.gif"
+    },
+    {
+        name: "ornamented shield",
+        def: 22,
+        weight: 72.0,
+        imageSrc: "/assets/imgs/shields/ornamented_shield.gif"
+    },
+    {
+        name: "phoenix shield",
+        def: 34,
+        weight: 35.0,
+        imageSrc: "/assets/imgs/shields/phoenix_shield.gif"
+    },
+    {
+        name: "plate shield",
+        def: 17,
+        weight: 65.0,
+        imageSrc: "/assets/imgs/shields/plate_shield.gif"
+    },
+    {
+        name: "rose shield",
+        def: 27,
+        weight: 52.0,
+        imageSrc: "/assets/imgs/shields/rose_shield.gif"
+    },
+    {
+        name: "scarab shield",
+        def: 25,
+        weight: 47.0,
+        imageSrc: "/assets/imgs/shields/scarab_shield.gif"
+    },
+    {
+        name: "shield of honour",
+        def: 33,
+        weight: 54.0,
+        imageSrc: "/assets/imgs/shields/shield_of_honour.gif"
+    },
+    {
+        name: "steel shield",
+        def: 21,
+        weight: 69.0,
+        imageSrc: "/assets/imgs/shields/steel_shield.gif"
+    },
+    {
+        name: "studded shield",
+        def: 15,
+        weight: 59.0,
+        imageSrc: "/assets/imgs/shields/studded_shield.gif"
+    },
+    {
+        name: "tempest shield",
+        def: 36,
+        weight: 51.0,
+        imageSrc: "/assets/imgs/shields/tempest_shield.gif"
+    },
+    {
+        name: "tower shield",
+        def: 32,
+        weight: 88.0,
+        imageSrc: "/assets/imgs/shields/tower_shield.gif"
+    },
+    {
+        name: "vampire shield",
+        def: 34,
+        weight: 38.0,
+        imageSrc: "/assets/imgs/shields/vampire_shield.gif"
+    },
+    {
+        name: "viking shield",
+        def: 22,
+        weight: 66.0,
+        imageSrc: "/assets/imgs/shields/viking_shield.gif"
+    },
+    {
+        name: "wooden shield",
+        def: 14,
+        weight: 40.0,
+        imageSrc: "/assets/imgs/shields/wooden_shield.gif"
+    }
+]
+
+module.exports = shields
