144055c2fadcb5437453b1768310aeb16d18e2fe
Norton Glitz <norton.glitz@gmail.com>
2023-12-26 03:21:52 -0300

    inserted loading ui

--- app/(pages)/characters/[displayName]/table.tsx
+++ app/(pages)/characters/[displayName]/table.tsx
@@ -12,7 +12,7 @@ interface TableProps {
 
 const Table: React.FC<TableProps> = ({ displayName }) => {
 
-    const { character, isLoading } = useCharacter(displayName)
+    const { character } = useCharacter(displayName)
 
     function capitalizeFirstLetter(string: string) {
         return string.charAt(0).toUpperCase() + string.slice(1)
@@ -20,157 +20,182 @@ const Table: React.FC<TableProps> = ({ displayName }) => {
 
     return (
         <div className="bg-stone-800 p-4 rounded-3xl border border-stone-600/30">
-            {character && (
-                <table>
-                    <caption
+            <table>
+                <caption
+                    className="
+                    font-yatra-one 
+                    text-2xl 
+                    whitespace-nowrap 
+                    text-yellow-200 
+                ">
+                    Character Information
+                    <hr className="border-stone-700 mt-2 mb-4" />
+                </caption>
+                <tbody
+                    className="
+                        [&>tr>th]:text-left 
+                        [&>tr>th]:p-2
+                    "
+                >
+                    <tr
                         className="
-                        font-yatra-one 
-                        text-2xl 
-                        whitespace-nowrap 
-                        text-yellow-200 
-                    ">
-                        Character Information
-                        <hr className="border-stone-700 mt-2 mb-4" />
-                    </caption>
-                    <tbody
-                        className="
-                            [&>tr>th]:text-left 
-                            [&>tr>th]:p-2
+                            bg-stone-300/10
+                            hover:outline
+                            hover:-outline-offset-1
+                            hover:outline-stone-400/80
                         "
                     >
-                        <tr
-                            className="
-                                bg-stone-300/10
-                                hover:outline
-                                hover:-outline-offset-1
-                                hover:outline-stone-400/80
-                            "
-                        >
-                            <th>
-                                Name
-                            </th>
-                            <td>
-                                {character.displayName}
-                            </td>
-                        </tr>
-                        <tr
-                            className="
-                                bg-stone-400/10
-                                hover:outline
-                                hover:-outline-offset-1
-                                hover:outline-stone-400/80
-                            "
-                        >
-                            <th>
-                                Status
-                            </th>
-                            <td>
-                                <div className="flex items-center">
-                                    {character.online ?
+                        <th>
+                            Name
+                        </th>
+                        <td>
+                            {
+                                !character ?
+                                    <div className="w-[5rem] bg-stone-500/30 animate-pulse h-[0.875rem] rounded-full" />
+                                    :
+                                    character.displayName
+                            }
+                        </td>
+                    </tr>
+                    <tr
+                        className="
+                            bg-stone-400/10
+                            hover:outline
+                            hover:-outline-offset-1
+                            hover:outline-stone-400/80
+                        "
+                    >
+                        <th>
+                            Status
+                        </th>
+                        <td>
+                            <div className="flex items-center">
+                                {!character ?
+                                    <div className="w-[5rem] bg-stone-500/30 animate-pulse h-[0.875rem] rounded-full" />
+                                    :
+                                    character.online ?
                                         <span className="text-tibia-green">Online</span>
                                         :
                                         <span className="text-red-400">Offline</span>
-                                    }
-                                </div>
-                            </td>
-                        </tr>
-                        <tr
-                            className="
-                                bg-stone-300/10
-                                hover:outline
-                                hover:-outline-offset-1
-                                hover:outline-stone-400/80
-                            "
-                        >
-                            <th>
-                                Level
-                            </th>
-                            <td>
-                                {character.level}
-                            </td>
-                        </tr>
-                        <tr
-                            className="
-                                bg-stone-400/10
-                                hover:outline
-                                hover:-outline-offset-1
-                                hover:outline-stone-400/80
-                            "
-                        >
-                            <th>
-                                Sex
-                            </th>
-                            <td className="capitalize">
-                                {getSexName(character.sex)}
-                            </td>
-                        </tr>
-                        <tr
-                            className="
-                                bg-stone-300/10
-                                hover:outline
-                                hover:-outline-offset-1
-                                hover:outline-stone-400/80
-                            "
-                        >
-                            <th>
-                                Vocation
-                            </th>
-                            <td className="capitalize">
-                                {getVocationName(character.vocation)}
-                            </td>
-                        </tr>
-                        <tr
-                            className="
-                                bg-stone-400/10
-                                hover:outline
-                                hover:-outline-offset-1
-                                hover:outline-stone-400/80
-                            "
-                        >
-                            <th>
-                                Residence
-                            </th>
-                            <td className="capitalize">
-                                {getCityName(character.residence)}
-                            </td>
-                        </tr>
-                        <tr
-                            className="
-                                bg-stone-300/10
-                                hover:outline
-                                hover:-outline-offset-1
-                                hover:outline-stone-400/80
-                            "
-                        >
-                            <th>
-                                Premium
-                            </th>
-                            <td className="text-sm">
-                                {character.premium ?
+                                }
+                            </div>
+                        </td>
+                    </tr>
+                    <tr
+                        className="
+                            bg-stone-300/10
+                            hover:outline
+                            hover:-outline-offset-1
+                            hover:outline-stone-400/80
+                        "
+                    >
+                        <th>
+                            Level
+                        </th>
+                        <td>
+                            {!character ? <div className="w-[5rem] bg-stone-500/30 animate-pulse h-[0.875rem] rounded-full" /> : character.level}
+                        </td>
+                    </tr>
+                    <tr
+                        className="
+                            bg-stone-400/10
+                            hover:outline
+                            hover:-outline-offset-1
+                            hover:outline-stone-400/80
+                        "
+                    >
+                        <th>
+                            Sex
+                        </th>
+                        <td className="capitalize">
+                            {!character ? <div className="w-[5rem] bg-stone-500/30 animate-pulse h-[0.875rem] rounded-full" /> : getSexName(character.sex)}
+                        </td>
+                    </tr>
+                    <tr
+                        className="
+                            bg-stone-300/10
+                            hover:outline
+                            hover:-outline-offset-1
+                            hover:outline-stone-400/80
+                        "
+                    >
+                        <th>
+                            Vocation
+                        </th>
+                        <td className="capitalize">
+                            {!character ?
+                                <div className="w-[5rem] bg-stone-500/30 animate-pulse h-[0.875rem] rounded-full" /> :
+                                getVocationName(character.vocation)
+                            }
+                        </td>
+                    </tr>
+                    <tr
+                        className="
+                            bg-stone-400/10
+                            hover:outline
+                            hover:-outline-offset-1
+                            hover:outline-stone-400/80
+                        "
+                    >
+                        <th>
+                            Residence
+                        </th>
+                        <td className="capitalize">
+                            {!character ?
+                                <div className="w-[5rem] bg-stone-500/30 animate-pulse h-[0.875rem] rounded-full" />
+                                :
+                                getCityName(character.residence)
+                            }
+                        </td>
+                    </tr>
+                    <tr
+                        className="
+                            bg-stone-300/10
+                            hover:outline
+                            hover:-outline-offset-1
+                            hover:outline-stone-400/80
+                        "
+                    >
+                        <th>
+                            Premium
+                        </th>
+                        <td className="text-sm">
+                            {!character ?
+                                <div className="w-[5rem] bg-stone-500/30 animate-pulse h-[0.875rem] rounded-full" />
+                                :
+                                character.premium ?
                                     <span className="text-tibia-green"><FaCheck /></span>
                                     :
                                     <span className="text-red-400"><FaX /></span>
-                                }
-                            </td>
-                        </tr>
-                        <tr
-                            className="
-                                bg-stone-400/10
-                                hover:outline
-                                hover:-outline-offset-1
-                                hover:outline-stone-400/80
-                            "
-                        >
-                            <th>
-                                {character.online ? "Playing for" : "Offline for"}
-                            </th>
-                            <td className="text-sm">
-                                {capitalizeFirstLetter(formatDistanceToNow(new Date(character.onlineUpdatedAt)))}
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            )}
+                            }
+                        </td>
+                    </tr>
+                    <tr
+                        className="
+                            bg-stone-400/10
+                            hover:outline
+                            hover:-outline-offset-1
+                            hover:outline-stone-400/80
+                        "
+                    >
+                        <th>
+                            {!character ?
+                                <div className="w-[5rem] bg-stone-500/30 animate-pulse h-[0.875rem] rounded-full" />
+                                :
+                                character.online ? "Playing for" : "Offline for"
+                            }
+                        </th>
+                        <td className="text-sm">
+                            {
+                                !character ?
+                                    <div className="w-[5rem] bg-stone-500/30 animate-pulse h-[0.875rem] rounded-full" />
+                                    :
+                                    capitalizeFirstLetter(formatDistanceToNow(new Date(character.onlineUpdatedAt)))
+                            }
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
