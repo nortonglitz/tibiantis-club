1cf6aad3d4baa1a0cf7961e1739ddc5fef9ea615
Norton Glitz <norton.glitz@gmail.com>
2023-12-29 21:23:21 -0300

    trying new error approach

--- app/api/crons/getPlayersOnline/route.ts
+++ app/api/crons/getPlayersOnline/route.ts
@@ -121,22 +121,138 @@ export async function GET() {
             }
         })
 
-        /* Players that logged in */
+        try {
 
-        if (newPlayers.length > 0) {
+            /* Players that logged in */
 
-            newPlayers.forEach(async ({ displayName, level, vocation }) => {
+            if (newPlayers.length > 0) {
 
-                const characterExists = await prisma.character.findUnique({ where: { name: displayName.toLowerCase() } })
+                newPlayers.forEach(async ({ displayName, level, vocation }) => {
 
-                /* Create new character if it does not exists */
+                    const characterExists = await prisma.character.findUnique({ where: { name: displayName.toLowerCase() } })
 
-                if (!characterExists) {
-                    const playerPageURL = `https://tibiantis.online/?page=character&name=${displayName.replaceAll(' ', '+')}`
+                    /* Create new character if it does not exists */
+
+                    if (!characterExists) {
+                        const playerPageURL = `https://tibiantis.online/?page=character&name=${displayName.replaceAll(' ', '+')}`
+                        const response = await fetch(playerPageURL, { cache: 'no-store' })
+
+                        if (!response.ok) {
+                            throw new Error(`Failed to reach Tibiantis during ${displayName} creation.`)
+                        }
+
+                        const htmlString = await response.text()
+                        const $ = cheerio.load(htmlString)
+
+                        const blockMsg = $('body').text()
+
+                        if (blockMsg.includes("You are currently blocked.")) {
+                            throw new Error(`Tibiantis got blocked during ${displayName} creation.`)
+                        }
+
+                        const sex = $('td:contains("Sex:")').next().text()
+                        const residence = $('td:contains("Residence:")').next().text()
+                        const accountStatus = $('td:contains("Account Status:")').next().text()
+
+                        if (sex.length < 1 || residence.length < 1 || accountStatus.length < 1) {
+                            console.error(`Failed to parse data on ${displayName} creation.`)
+                            return false
+                        }
+
+                        const newCharacter = await prisma.character.create({
+                            data: {
+                                name: displayName.toLocaleLowerCase(),
+                                displayName: displayName,
+                                sex: getSexNumber(sex),
+                                vocation,
+                                level: Number(level),
+                                residence: getCityNumber(residence),
+                                premium: isPremiumAccount(accountStatus),
+                                online: true,
+                                onlineUpdatedAt: new Date()
+                            }
+                        })
+
+
+                        await prisma.playerSessionDay.create({
+                            data: {
+                                characterId: newCharacter.id,
+                                endLevel: Number(level),
+                                startLevel: Number(level),
+                                expGained: 0,
+                                playtime: 0
+                            }
+                        })
+
+                    } else {
+
+                        /* Update this character to online, if it exists */
+                        await prisma.character.update({
+                            where: { id: characterExists.id },
+                            data: {
+                                online: true,
+                                onlineUpdatedAt: new Date()
+                            }
+                        })
+
+                        /* Get last session during server day */
+
+                        const lastSessionDay = await prisma.playerSessionDay.findFirst({
+                            where: {
+                                AND: [
+                                    { characterId: characterExists.id },
+                                    {
+                                        createdAt: {
+                                            gte: serverStart,
+                                            lte: serverEnd
+                                        }
+                                    }
+                                ]
+                            }
+                        })
+
+                        if (!lastSessionDay) {
+                            await prisma.playerSessionDay.create({
+                                data: {
+                                    characterId: characterExists.id,
+                                    endLevel: Number(level),
+                                    startLevel: Number(level),
+                                    expGained: 0,
+                                    playtime: 0
+                                }
+                            })
+                        }
+                    }
+                })
+            }
+        } catch (err: any) {
+            console.error(err.message)
+        }
+
+        try {
+            if (oldPlayersOnline && oldPlayersOnline.length > 0) {
+
+                /* What still on the old array are players that logged off */
+
+                oldPlayersOnline.forEach(async ({ id, displayName, onlineUpdatedAt }) => {
+
+                    /* Create session for old player */
+
+                    const oldPlayerLastSession = await prisma.playerSession.create({
+                        data: {
+                            characterId: id,
+                            startedAt: onlineUpdatedAt
+                        }
+                    })
+
+                    /* Fetch old player data */
+
+                    const playerPageURL = `https://tibiantis.online/?page=character&name=${displayName.split(' ').join('+')}`
                     const response = await fetch(playerPageURL, { cache: 'no-store' })
 
+                    /* Stops if fetch failed  */
                     if (!response.ok) {
-                        throw new Error(`Failed to reach Tibiantis during ${displayName} creation.`)
+                        throw new Error(`Failed to reach Tibiantis during ${displayName} update.`)
                     }
 
                     const htmlString = await response.text()
@@ -144,176 +260,69 @@ export async function GET() {
 
                     const blockMsg = $('body').text()
 
+                    /* Stops if site is blocked  */
+
                     if (blockMsg.includes("You are currently blocked.")) {
-                        throw new Error(`Tibiantis got blocked during ${displayName} creation.`)
+                        throw new Error(`Tibiantis got blocked during ${displayName} update.`)
                     }
 
                     const sex = $('td:contains("Sex:")').next().text()
                     const residence = $('td:contains("Residence:")').next().text()
+                    const level = $('td:contains("Level:")').next().text()
+                    const vocation = $('td:contains("Vocation:")').next().text()
                     const accountStatus = $('td:contains("Account Status:")').next().text()
 
-                    if (sex.length < 1 || residence.length < 1 || accountStatus.length < 1) {
-                        console.error(`Failed to parse data on ${displayName} creation.`)
+                    if (sex.length < 1 || residence.length < 1 || accountStatus.length < 1 || vocation.length < 1 || level.length < 1) {
+                        console.error(`Failed to parse ${displayName} data.`)
                         return false
                     }
 
-                    const newCharacter = await prisma.character.create({
+                    /* Get his last day session, if he was online he had one session already created on this day */
+
+                    const oldPlayerSessionDay = await prisma.playerSessionDay.findFirst({
+                        where: { characterId: id },
+                        orderBy: {
+                            id: 'desc'
+                        }
+                    })
+
+                    /* Time he played last session */
+
+                    const lastPlaytime = differenceInMinutes(oldPlayerLastSession.endedAt, oldPlayerLastSession.startedAt)
+
+                    /* Update his day session */
+
+                    if (oldPlayerSessionDay) {
+                        await prisma.playerSessionDay.update({
+                            where: { id: oldPlayerSessionDay.id },
+                            data: {
+                                endLevel: Number(level),
+                                expGained: getExpGained(oldPlayerSessionDay.startLevel, Number(level)),
+                                playtime: oldPlayerSessionDay.playtime + lastPlaytime
+                            }
+                        })
+                    } else {
+                        console.error(`Failed to update ${displayName} day session.`)
+                    }
+
+                    /* Update character */
+
+                    await prisma.character.update({
+                        where: { id },
                         data: {
-                            name: displayName.toLocaleLowerCase(),
-                            displayName: displayName,
                             sex: getSexNumber(sex),
-                            vocation,
+                            vocation: getVocationNumber(vocation),
                             level: Number(level),
                             residence: getCityNumber(residence),
                             premium: isPremiumAccount(accountStatus),
-                            online: true,
+                            online: false,
                             onlineUpdatedAt: new Date()
                         }
                     })
-
-
-                    await prisma.playerSessionDay.create({
-                        data: {
-                            characterId: newCharacter.id,
-                            endLevel: Number(level),
-                            startLevel: Number(level),
-                            expGained: 0,
-                            playtime: 0
-                        }
-                    })
-
-                } else {
-
-                    /* Update this character to online, if it exists */
-                    await prisma.character.update({
-                        where: { id: characterExists.id },
-                        data: {
-                            online: true,
-                            onlineUpdatedAt: new Date()
-                        }
-                    })
-
-                    /* Get last session during server day */
-
-                    const lastSessionDay = await prisma.playerSessionDay.findFirst({
-                        where: {
-                            AND: [
-                                { characterId: characterExists.id },
-                                {
-                                    createdAt: {
-                                        gte: serverStart,
-                                        lte: serverEnd
-                                    }
-                                }
-                            ]
-                        }
-                    })
-
-                    if (!lastSessionDay) {
-                        await prisma.playerSessionDay.create({
-                            data: {
-                                characterId: characterExists.id,
-                                endLevel: Number(level),
-                                startLevel: Number(level),
-                                expGained: 0,
-                                playtime: 0
-                            }
-                        })
-                    }
-                }
-            })
-        }
-
-        /* What still on the old array are players that logged off */
-
-        if (oldPlayersOnline && oldPlayersOnline.length > 0) {
-
-            oldPlayersOnline.forEach(async ({ id, displayName, onlineUpdatedAt }) => {
-
-                /* Create session for old player */
-
-                const oldPlayerLastSession = await prisma.playerSession.create({
-                    data: {
-                        characterId: id,
-                        startedAt: onlineUpdatedAt
-                    }
                 })
-
-                /* Fetch old player data */
-
-                const playerPageURL = `https://tibiantis.online/?page=character&name=${displayName.split(' ').join('+')}`
-                const response = await fetch(playerPageURL, { cache: 'no-store' })
-
-                /* Stops if fetch failed  */
-                if (!response.ok) {
-                    throw new Error(`Failed to reach Tibiantis during ${displayName} update.`)
-                }
-
-                const htmlString = await response.text()
-                const $ = cheerio.load(htmlString)
-
-                const blockMsg = $('body').text()
-
-                /* Stops if site is blocked  */
-
-                if (blockMsg.includes("You are currently blocked.")) {
-                    throw new Error(`Tibiantis got blocked during ${displayName} update.`)
-                }
-
-                const sex = $('td:contains("Sex:")').next().text()
-                const residence = $('td:contains("Residence:")').next().text()
-                const level = $('td:contains("Level:")').next().text()
-                const vocation = $('td:contains("Vocation:")').next().text()
-                const accountStatus = $('td:contains("Account Status:")').next().text()
-
-                if (sex.length < 1 || residence.length < 1 || accountStatus.length < 1 || vocation.length < 1 || level.length < 1) {
-                    console.error(`Failed to parse ${displayName} data.`)
-                    return false
-                }
-
-                /* Get his last day session, if he was online he had one session already created on this day */
-
-                const oldPlayerSessionDay = await prisma.playerSessionDay.findFirst({
-                    where: { characterId: id },
-                    orderBy: {
-                        id: 'desc'
-                    }
-                })
-
-                /* Time he played last session */
-
-                const lastPlaytime = differenceInMinutes(oldPlayerLastSession.endedAt, oldPlayerLastSession.startedAt)
-
-                /* Update his day session */
-
-                if (oldPlayerSessionDay) {
-                    await prisma.playerSessionDay.update({
-                        where: { id: oldPlayerSessionDay.id },
-                        data: {
-                            endLevel: Number(level),
-                            expGained: getExpGained(oldPlayerSessionDay.startLevel, Number(level)),
-                            playtime: oldPlayerSessionDay.playtime + lastPlaytime
-                        }
-                    })
-                } else {
-                    console.error(`Failed to update ${displayName} day session.`)
-                }
-
-                /* Update character */
-
-                await prisma.character.update({
-                    where: { id },
-                    data: {
-                        sex: getSexNumber(sex),
-                        vocation: getVocationNumber(vocation),
-                        level: Number(level),
-                        residence: getCityNumber(residence),
-                        premium: isPremiumAccount(accountStatus),
-                        online: false,
-                        onlineUpdatedAt: new Date()
-                    }
-                })
-            })
+            }
+        } catch (err: any) {
+            console.error(err.message)
         }
 
         return Response.json({
