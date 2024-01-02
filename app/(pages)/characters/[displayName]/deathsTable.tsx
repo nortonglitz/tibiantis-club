93ab990eebfcdcf9afb4beb6e1bc470fc417a4e6
Norton Glitz <norton.glitz@gmail.com>
2024-01-02 18:03:22 -0300

    death table working

--- app/(pages)/characters/[displayName]/deathsTable.tsx
+++ app/(pages)/characters/[displayName]/deathsTable.tsx
@@ -71,65 +71,91 @@ const DeathsTable: React.FC<DeathsTableProps> = ({ displayName }) => {
                         text-center
                     "
                 >
-                    {deaths && deaths.map(({ date, cause, killers, creature, field, level }, i) => (
-                        <tr
-                            key={i}
-                            className="
-                                [&>td]:py-2
-                                odd:bg-stone-300/10 
-                                even:bg-stone-400/10
-                                hover:outline
-                                hover:-outline-offset-1
-                                hover:outline-stone-400/80
-                            "
-                        >
-                            <td>{formatDistanceToNow(date)} ago</td>
-                            <td>
-                                {level}
-                            </td>
-                            <>
-                                {cause === 0 && creature && (
-                                    <td>
-                                        {<img
-                                            className="m-auto"
-                                            src={`/assets/imgs/beastiary/${getCreatureName(creature).replaceAll(' ', '_')}.gif`}
-                                            alt={getCreatureName(creature)}
-                                        />}
+                    {isLoading ?
+                        <>
+                            {[1, 2, 3].map((key) => (
+                                <tr className="odd:bg-stone-300/10 even:bg-stone-400/10" key={key}>
+                                    <td className="py-2"><div className="w-[5rem] m-auto bg-stone-500/30 animate-pulse h-[0.875rem] rounded-full" /></td>
+                                    <td className="py-2"><div className="w-[2rem] m-auto bg-stone-500/30 animate-pulse h-[0.875rem] rounded-full" /></td>
+                                    <td className="py-2"><div className="w-[5rem] m-auto bg-stone-500/30 animate-pulse h-[0.875rem] rounded-full" /></td>
+                                </tr>
+                            ))}
+                        </>
+                        :
+                        error ?
+                            <tr>
+                                <td colSpan={3} className="text-center py-2 bg-stone-300/10">Something went wrong.</td>
+                            </tr>
+                            :
+                            deaths.length < 1 ?
+                                <tr>
+                                    <td colSpan={3} className="text-center py-2 bg-stone-300/10">
+                                        No deaths has been found.
                                     </td>
-                                )}
-                                {cause === 1 && (
-                                    <td>
-                                        {killers.length > 1 ?
+                                </tr>
+                                :
+                                <>
+                                    {deaths && deaths.map(({ date, cause, killers, creature, field, level }, i) => (
+                                        <tr
+                                            key={i}
+                                            className="
+                                                [&>td]:py-2
+                                                odd:bg-stone-300/10 
+                                                even:bg-stone-400/10
+                                                hover:outline
+                                                hover:-outline-offset-1
+                                                hover:outline-stone-400/80
+                                            "
+                                        >
+                                            <td>{formatDistanceToNow(date)} ago</td>
+                                            <td>
+                                                {level}
+                                            </td>
                                             <>
-                                                <LinkText href={`/characters/${killers[0].displayName.replaceAll(' ', '_')}`}>
-                                                    {killers[0].displayName}
-                                                </LinkText>
-                                                {" and "}
-                                                <LinkText href={`/characters/${killers[0].displayName.replaceAll(' ', '_')}`}>
-                                                    {killers[1].displayName}
-                                                </LinkText>
+                                                {cause === 0 && creature && (
+                                                    <td>
+                                                        {<img
+                                                            className="m-auto"
+                                                            src={`/assets/imgs/beastiary/${getCreatureName(creature).replaceAll(' ', '_')}.gif`}
+                                                            alt={getCreatureName(creature)}
+                                                        />}
+                                                    </td>
+                                                )}
+                                                {cause === 1 && (
+                                                    <td>
+                                                        {killers.length > 1 ?
+                                                            <>
+                                                                <LinkText href={`/characters/${killers[0].displayName.replaceAll(' ', '_')}`}>
+                                                                    {killers[0].displayName}
+                                                                </LinkText>
+                                                                {" and "}
+                                                                <LinkText href={`/characters/${killers[0].displayName.replaceAll(' ', '_')}`}>
+                                                                    {killers[1].displayName}
+                                                                </LinkText>
+                                                            </>
+                                                            :
+                                                            <>
+                                                                <LinkText href={`/characters/${killers[0].displayName.replaceAll(' ', '_')}`}>
+                                                                    {killers[0].displayName}
+                                                                </LinkText>
+                                                            </>
+                                                        }
+                                                    </td>
+                                                )}
+                                                {cause === 2 && field !== null && (
+                                                    <td>
+                                                        {<img
+                                                            className="m-auto"
+                                                            src={`/assets/imgs/fields/${getFieldName(field).replaceAll(' ', '_')}.gif`}
+                                                            alt={getFieldName(field)}
+                                                        />}
+                                                    </td>
+                                                )}
                                             </>
-                                            :
-                                            <>
-                                                <LinkText href={`/characters/${killers[0].displayName.replaceAll(' ', '_')}`}>
-                                                    {killers[0].displayName}
-                                                </LinkText>
-                                            </>
-                                        }
-                                    </td>
-                                )}
-                                {cause === 2 && field !== null && (
-                                    <td>
-                                        {<img
-                                            className="m-auto"
-                                            src={`/assets/imgs/fields/${getFieldName(field).replaceAll(' ', '_')}.gif`}
-                                            alt={getFieldName(field)}
-                                        />}
-                                    </td>
-                                )}
-                            </>
-                        </tr>
-                    ))}
+                                        </tr>
+                                    ))}
+                                </>
+                    }
                 </tbody>
             </table>
         </div>
