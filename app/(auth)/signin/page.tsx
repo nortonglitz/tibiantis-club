a418c40030e8baf87267b7288e05e84bf8a34380
Norton Glitz <norton.glitz@gmail.com>
2024-02-02 18:24:19 -0300

    insert login title on page

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,4 +1,9 @@
 import { SignInForm } from "./signInForm"
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'Login - Tibiantis Club',
+}
 
 const SignInPage = () => {
     return (
